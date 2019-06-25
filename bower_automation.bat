a6120d83c73d1e15942dba4910ffdd521c240dd0
Banee Ishaque K <Baneeishaque@gmail.com>
2019-06-26 03:47:49 +0530

    bower

--- bower_automation.bat
+++ bower_automation.bat
@@ -1,15 +1,53 @@
-ECHO Generating file list... | tee -a bower_automation-results.txt
-DIR %1 /B /S | FINDSTR /E bower.json > bower_project-files.list
+SET script_path=%CD%
+REM ECHO %script_path%
+REM PAUSE
+REM SETLOCAL
+REM ECHO %%~dp0
+REM PAUSE
+ECHO Generating file list... | tee bower_automation-results.txt
+CAT bower_automation-results.txt
+ECHO.
+REM PAUSE
+REM DIR %1
+REM PAUSE
+REM DIR %1 /B
+REM PAUSE
+REM DIR %1 /B /S
+REM PAUSE
+REM DIR %1 /B /S /A:-D
+REM PAUSE
+REM DIR %1 /B /S /A:-D | FINDSTR /E bower.json
+REM PAUSE
+REM DIR %1 /B /S /A:-D | FINDSTR /E bower.json | FINDSTR /V "\node_modules\ \lib\ \vendors\ "
+REM PAUSE
+DIR %1 /B /S /A:-D | FINDSTR /E bower.json | FINDSTR /V "\node_modules\ \lib\ \vendors\ \bower_components\ " > bower_project-files.list
+CAT bower_project-files.list
+ECHO.
+PAUSE
 ECHO File list generated successfully... | tee -a bower_automation-results.txt
+CAT bower_automation-results.txt
+ECHO.
 IF "%2"=="I" (PAUSE)
 ECHO. | tee -a bower_automation-results.txt
+CAT bower_automation-results.txt
+ECHO.
+REM PAUSE
 FOR /F "TOKENS=*" %%a IN ('TYPE bower_project-files.list') DO (
 	ECHO Processing %%a | tee -a bower_automation-results.txt
+	CAT bower_automation-results.txt
+	ECHO.
+	REM PAUSE
 	CD %%~dpa
+	REM PAUSE
 	bower install
-	CD %3
+	CD %script_path%
+	REM PAUSE
 	IF "%2"=="I" (PAUSE)
 )
 ECHO.
 ECHO Cleaning... | tee -a bower_automation-results.txt
-REM DEL project-files.list
+CAT bower_automation-results.txt
+ECHO.
+DEL bower_project-files.list
+DIR
+REM PAUSE
---
+++ recursive-npm-cleaner.bat
@@ -0,0 +1,64 @@
+@SETLOCAL enabledelayedexpansion
+@ECHO off
+ECHO Cleaning...
+for /d /r "%~dp0" %%a in (node_modules\) do if exist "%%a" rmdir /s /q "%%a"
+for /d /r "%~dp0" %%a in (.gradle\) do if exist "%%a" rmdir /s /q "%%a"
+for /d /r "%~dp0" %%a in (build\) do if exist "%%a" rmdir /s /q "%%a"
+GOTO end
+ECHO Generating file list...
+DIR /s /b > project-files.list
+ECHO File list generated successfully...
+pause
+ECHO Generating individual-gradle-builds file...
+DEL recursive-gradle-build-results.txt
+ECHO CD %%~d0 >individual-gradle-builds.bat
+ECHO ECHO. >>individual-gradle-builds.bat
+FOR /f "tokens=*" %%a IN ('findstr "gradlew.bat" project-files.list') DO (
+	ECHO CD %%~dpa >>individual-gradle-builds.bat
+	ECHO ECHO Building %%~dpa ^| tee -a ..\recursive-gradle-build-results.txt>>individual-gradle-builds.bat
+	ECHO CALL gradlew.bat build 2^>^&1 ^| tee -a ..\recursive-gradle-build-results.txt >>individual-gradle-builds.bat
+	ECHO ECHO Build finished for %%~dpa ^&^& ECHO. ^&^& ECHO. ^&^& ECHO. ^| tee -a ..\recursive-gradle-build-results.txt>>individual-gradle-builds.bat
+	ECHO pause >>individual-gradle-builds.bat
+)
+ECHO CD .. >>individual-gradle-builds.bat
+
+SET INTEXTFILE=individual-gradle-builds.bat
+SET OUTTEXTFILE=individual-gradle-builds_out.bat
+SET SEARCHTEXT=ECHO Building %~dp0
+SET REPLACETEXT=ECHO Building 
+SET OUTPUTLINE=
+
+FOR /f "tokens=1,* delims=¶" %%A IN ( '"TYPE %INTEXTFILE%"') DO (
+	SET string=%%A
+	SET modified=!string:%SEARCHTEXT%=%REPLACETEXT%!
+
+	ECHO !modified! >> %OUTTEXTFILE%
+)
+DEL %INTEXTFILE%
+RENAME %OUTTEXTFILE% %INTEXTFILE%
+
+SET INTEXTFILE=individual-gradle-builds.bat
+SET OUTTEXTFILE=individual-gradle-builds_out.bat
+SET SEARCHTEXT=ECHO Build finished for %~dp0
+SET REPLACETEXT=ECHO Build finished for 
+SET OUTPUTLINE=
+
+FOR /f "tokens=1,* delims=¶" %%A IN ( '"TYPE %INTEXTFILE%"') DO (
+	SET string=%%A
+	SET modified=!string:%SEARCHTEXT%=%REPLACETEXT%!
+
+	ECHO !modified! >> %OUTTEXTFILE%
+)
+DEL %INTEXTFILE%
+RENAME %OUTTEXTFILE% %INTEXTFILE%
+ECHO Individual-gradle-builds file generated successfully...
+pause
+ECHO Executing individual-gradle-builds file...
+CALL individual-gradle-builds.bat
+ECHO Execution of individual-gradle-builds file completed successfully...
+pause
+ECHO Cleaning...
+DEL individual-gradle-builds.bat
+DEL project-files.list
+
+:end
